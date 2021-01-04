60e66093dd3d369d9cd7d667b6fb93ef472c8cc0
H <leithhamish@gmail.com>
2021-01-04 19:36:35 +0000

    pageturns

--- js/card.js
+++ js/card.js
@@ -14,13 +14,13 @@ ScrollReveal().reveal('.message', { duration: 5000, delay: 000 });
 
 /* Handle Image Upload */
 
-var loadFile = function(event) {
-	var image = document.getElementById('img1');
+const loadFile = function(event) {
+	const image = document.getElementById('img1');
     image.src = URL.createObjectURL(event.target.files[0]);
     // document.getElementById('savebar').setAttribute('style', 'display: block;');
 };
-var loadFile2 = function(event) {
-	var image = document.getElementById('img2');
+const loadFile2 = function(event) {
+	const image = document.getElementById('img2');
     image.src = URL.createObjectURL(event.target.files[0]);
     // document.getElementById('savebar').setAttribute('style', 'display: block;');
 };
@@ -29,7 +29,7 @@ function startEdit() {
     document.getElementById('editprompt').setAttribute('style','display: none');
     document.getElementById('savebar').setAttribute('style', 'display: flex;');
     document.querySelectorAll('.custom-file-upload').forEach(item => {
-        item.setAttribute('style', 'display: inline-block;');
+        item.setAttribute('style', 'display: block;');
     });
     document.querySelectorAll('.message').forEach(item => {
         item.readOnly = false;
@@ -42,3 +42,43 @@ function startEdit() {
         });
     });
 }
+
+// Flip card
+
+document.querySelectorAll('.paper').forEach(item => {
+
+    item.addEventListener("click", function(event) {
+        console.log(event.target);
+        const msg = document.getElementById('message');
+        if (msg === event.target && msg.readOnly === false) {
+            
+        }
+        else{
+        
+        //console.log("Flipping page");
+        index = this.style.zIndex;
+        //console.log('Original index:'+index);
+        newIndex = 4-index;
+        //console.log('New index:'+newIndex);
+
+        el = this;
+        //console.log(el);
+
+        if (newIndex <= index) {
+            this.classList.toggle('is-flipped');
+            setTimeout(function(){ el.style.zIndex = newIndex; }, 500);
+            console.log("page forward");
+        }
+        else {
+            let t0 = Date.now();
+            el.style.zIndex = newIndex;
+            console.log(Date.now()-t0);
+            setTimeout(function(){ el.classList.toggle('is-flipped')}, 500);
+            console.log("page back");
+        }
+
+        
+        }
+    })
+
+});
